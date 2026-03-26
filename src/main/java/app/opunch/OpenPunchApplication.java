d1d747d9eb4879c75649ee48239bd7ed8072cb99
Marcel Garcia <marcelgb.dev@gmail.com>
2026-03-26 21:39:09 +0100

    git ignore

---
+++ src/main/java/app/opunch/OpenPunchApplication.java
@@ -0,0 +1,13 @@
+package app.opunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenPunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenPunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/opunch/OpenPunchApplicationTests.java
@@ -0,0 +1,13 @@
+package app.opunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenPunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
