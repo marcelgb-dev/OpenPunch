4af340b6925a92118f222044fcb31a6418bbacf2
Marcel Garcia <marcelgb.dev@gmail.com>
2026-04-22 14:07:00 +0200

    - Added comment for returnOptional method

--- src/main/java/app/opunch/repository/BaseRepository.java
+++ src/main/java/app/opunch/repository/BaseRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public interface BaseRepository {
 
     // Utilities
+    // Returns an empty Optional of type T if there are no results, or the full Optional with the results if they exist
     default <T> Optional<T> returnOptional(List<T> results) {
 
         if (results.isEmpty()) {
